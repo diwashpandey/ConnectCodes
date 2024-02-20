62a086dfa27bb6c3ae167e5ed109057eb6a529f1
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-02-20 18:55:39 +0545

    Added Security to room
    Feature has been added:
    1. User cannot join if not authenticated(will be redirected to login page).
    2. User cannot send message without joining the room.
    3. etc etc

--- ConnectCodes/rooms/urls.py
+++ ConnectCodes/rooms/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('<int:pk>/', views.getroompage, name='roompage'),
-    path('createnewroom/', views.getcreateroompage, name='createroompage')
+    path('createnewroom/', views.getcreateroompage, name='createroompage'),
+    path('joinroom/<str:pk>/', views.add_into_room, name='joinroom')
 ]
--- ConnectCodes/rooms/views.py
+++ ConnectCodes/rooms/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Message, Room, Topic
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 # Create your views here.
 
 def getroompage(request, pk):
@@ -10,13 +12,18 @@ def getroompage(request, pk):
         room = Room.objects.get(id=key)
         Message.objects.create(room = room, user = request.user, message = message)
     
+    
+
     room = Room.objects.get(id=key)
     room_members = room.members.all()
     messages = Message.objects.filter(room__id = key)
+    is_member = False
+    if request.user in room_members:
+        is_member = True
+    print("Is member:", is_member)
+    return render(request, "rooms/room.html", {'messages': messages, 'room' : room, 'room_members': room_members, "is_member": is_member})
 
-    return render(request, "rooms/room.html", {'messages': messages, 'room' : room, 'room_members': room_members})
-
-
+@login_required(login_url = "loginpage")
 def getcreateroompage(request):
     if request.method == "POST":
         data = request.POST
@@ -31,3 +38,10 @@ def getcreateroompage(request):
     
     topics = Topic.objects.all()
     return render(request, 'rooms/createroom.html',{'topics':topics})
+
+@login_required(login_url = "loginpage")
+def add_into_room(request, pk):
+    user = request.user
+    room = Room.objects.get(id = pk)
+    room.members.add(user)
+    return redirect('roompage', pk=pk)
