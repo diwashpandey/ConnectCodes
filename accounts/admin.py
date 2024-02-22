d956f87a615cc2be0a44816424d16ad2d3df46f4
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-02-22 23:12:38 +0545

    Just Database Reset

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,1 @@
 from django.contrib import admin
-from .models import CustomUser
-# Register your models here.
-
-admin.site.register(CustomUser)
--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,1 @@
 from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
-
-class CustomUser(User):
-    name = models.CharField(max_length = 50, default="ConnectCode User")
-    profile_photo = models.ImageField(upload_to="profile_photo/", default='default_profile_photo.jpg')
-
-    def __str__(self):
-        return self.username
