6e93f6e9e9236b22f01643cc6f082306940101d1
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-02-26 18:59:42 +0545

    Removed the theme mode glitch.
    Glitch was, user has to change the theme at every reload.
    So, used local storage to save the user choice with the help of JS.

--- static/js/themechanger.js
+++ static/js/themechanger.js
@@ -1,19 +1,36 @@
-themeSelection = document.querySelector("#theme_selection")
-moon = document.querySelector(".moon")
-sun = document.querySelector(".sun")
-body = document.querySelector('body')
-themeBox = document.querySelector('.theme_box')
+const themeSelection = document.querySelector("#theme_selection");
+const moon = document.querySelector(".moon");
+const sun = document.querySelector(".sun");
+const body = document.querySelector("body");
+const themeBox = document.querySelector(".theme_box");
 
-themeBox.addEventListener('click', (e)=>{
-    if (themeSelection.checked){
-        sun.classList.remove('active')
-        moon.classList.add('active')
-        body.classList.add('darktheme_appplied')
-    }
-    else{
-        moon.classList.remove('active')
-        sun.classList.add('active')
-        body.classList.remove('darktheme_appplied')
-    }
-})
+const theme = localStorage.getItem("theme");
 
+function setDark() {
+  sun.classList.remove("active");
+  moon.classList.add("active");
+  body.classList.add("darktheme_appplied");
+  localStorage.setItem("theme", "dark");
+}
+
+function setLight() {
+  moon.classList.remove("active");
+  sun.classList.add("active");
+  body.classList.remove("darktheme_appplied");
+  localStorage.setItem("theme", "light");
+}
+
+themeBox.addEventListener("click", (e) => {
+  if (themeSelection.checked) {
+    setDark();
+  } else {
+    setLight();
+  }
+});
+
+if (theme != null) {
+  if (theme === "dark") {
+    themeSelection.checked = true;
+    setDark();
+  }
+}
