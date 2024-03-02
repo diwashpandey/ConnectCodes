cf33376e598909220879c7803a0d5ced28af4bb0
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-03-02 21:57:26 +0545

    Just made a basic websocket with JS in room page..
    Just wrote a WS connection in front-end in room page.
    - Updated Requirements.py
    - Other are minor edits.

--- ConnectCodes/settings.py
+++ ConnectCodes/settings.py
@@ -88,6 +88,14 @@ DATABASES = {
     }
 }
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
---
+++ static/js/connect_socket.js
@@ -0,0 +1,18 @@
+const url = "ws://127.0.0.1:8000/ws/chat/"
+const socket = new WebSocket(url)
+
+// It's just making and overlay for now... main work will be done later...
+
+console.log("Running..")
+
+socket.onopen = (e)=>{
+    console.log("Connection established with server...")
+}
+
+socket.onclose=(e)=>{
+    console.log("Connestion lost with server...")
+}
+
+socket.onmessage=(e)=>{
+    console.log("Received messege from the server")
+}
