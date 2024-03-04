8cafe3376f3afdfa5aaf006381aa36021e705c74
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-03-04 19:15:12 +0545

    JavaScript sends and receives the data, and then adds to chat feild after building the Element completely.
    I've worked with JS WebSocket and JS DOM here.
    
    - An Event Listener was added, which transmits the data to the WebSocket upon submit.
    - After receiving data back from the server, JS creates the entire element using the DOM and appends it to the chat field.

--- static/js/connect_socket.js
+++ static/js/connect_socket.js
@@ -1,4 +1,10 @@
-const url = "ws://127.0.0.1:8000/ws/chat/diwash/"
+const roomId = JSON.parse(document.getElementById("room_id").text)
+const form = document.querySelector("#form")
+const messageBox = document.querySelector(".message_box")
+const chats = document.querySelector(".chats")
+
+const url = `ws://127.0.0.1:8000/ws/chat/${roomId}/`
+
 const socket = new WebSocket(url)
 
 // It's just making and overlay for now... main work will be done later...
@@ -15,4 +21,48 @@ socket.onclose=(e)=>{
 
 socket.onmessage=(e)=>{
     console.log("Received messege from the server")
+
+    // Creates a list-item to be filled with data and pushed into ul(.chats)
+    let li = document.createElement("li")
+
+
+    // Creates the first div section for list-item
+    let div1 = document.createElement("div")
+    div1.classList.add("section_1")
+    //Creates the img tag for profile picture and puts the source link
+    let img = document.createElement("img")
+    img.src = ""
+
+    div1.appendChild(img) //Finally append the well created img element inside the div
+    
+
+
+    // Create the second dic section for list-item
+    let div2 = document.createElement("div")
+    div2.classList.add("section_2")
+    // Create the anchor-point for username along with the username in it
+    let a = document.createElement("a")
+    a.href = "" //Will add the link here
+    // Create the p tag and add the reveived messege to it.
+    let p = document.createElement("p")
+    p.innerHTML = e.data
+
+
+    //Append username and messege inside the section_2 div 
+    div2.appendChild(a)
+    div2.appendChild(p)
+
+    
+    //Add both sections(div) into the li
+    li.appendChild(div1)
+    li.appendChild(div2)
+
+    //Finally add the li to the .chats
+    chats.appendChild(li)
+
 }
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    socket.send(messageBox.value)
+})
