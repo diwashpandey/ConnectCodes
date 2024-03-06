1281fe8eb256b321103e34162d47d787080c34b4
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-03-06 21:18:52 +0545

    Received WebSocket text data in backend.
    Nothing Much has been done.
    - No any work with database has been done.

--- accounts/consumers.py
+++ accounts/consumers.py
@@ -1,14 +1,25 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.exceptions import StopConsumer
+from channels.db import database_sync_to_async
+import json
+from .models import UserAccount
 
 
-class CommitConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class CommitConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
 
-    def receive(self, text_data=None, bytes_data=None):
-        self.send(text_data=text_data)
+    async def receive(self, text_data=None, bytes_data=None):
+        if self.scope["user"].is_authenticated:
+            data = json.loads(text_data)
+            username = data["username"]
+            commit = data["commit"]
 
-    def disconnect(self):
+            await self.send(text_data=text_data)
+
+    async def disconnect(self):
         raise StopConsumer()
+    
+    
+    
         
