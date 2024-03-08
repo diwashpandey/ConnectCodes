00af1bbdc74dc70dfaa42f49323c91d624099bc5
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-03-08 10:07:15 +0545

    Work in Frontend WebSocket. A little bit in server Consumer too.
    The JSON string is delivered to JS by the server.
    Parse it, then adjust the button's value and innerHTML accordingly.
    
    Minimal Work in Backend Consumer: unnecessary codes have been eliminated.
    Return the dict's JSON string rather than just the commit value.

--- accounts/consumers.py
+++ accounts/consumers.py
@@ -4,48 +4,48 @@ from channels.db import database_sync_to_async
 import json
 from .models import UserAccount
 
-
 class CommitConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
 
     async def receive(self, text_data=None, bytes_data=None):
-        print("Data received from the client..")
         if self.scope["user"].is_authenticated:
-            print("User is authenticated...")
             data = json.loads(text_data)
             username = data["username"]
             commit = data["commit"]
-            print("the commit is:",commit)
-            print("the username is:",username)
-
 
             try:
-                print("Inside try...")
                 # Get the full object of a authenticated user 
                 requested_user = await database_sync_to_async(UserAccount.objects.get)(username = self.scope["user"].username)
 
                 # Get the full object of a user-to-commit
                 user_to_commit = await database_sync_to_async(UserAccount.objects.get)(username=username)
                 
-                if commit == "unfollow":
-                    print("inside unfollow")
+                if commit == "Unfollow":
                     # Remove the requested_user's foreignkey from the user_to_commit 
                     await database_sync_to_async(requested_user.following.remove)(user_to_commit)
-                    text_data = "follow"
+                    text_data = json.dumps(
+                        {
+                            "new_commit":"Follow",
+                            "username":user_to_commit.username
+                        }
+                    )
+                    await self.send(text_data=text_data)
                     
-                elif commit == "follow":
-                    print("inside follow")
+                elif commit == "Follow":
                     # Add the requested_user's foreignkey to the user_to_commit 
                     await database_sync_to_async(requested_user.following.add)(user_to_commit)
-                    text_data = "unfollow"
-                await self.send(text_data=text_data)
-            except:
-                print("I'm inside except")
+                    text_data = json.dumps(
+                        {
+                            "new_commit":"Unfollow",
+                            "username":user_to_commit.username
+                        }
+                    )
+                    await self.send(text_data=text_data)
+            except Exception as E:
                 text_data = "Something error happened"
 
             self.send(text_data=text_data)
-
 
     async def disconnect(self, code):
         raise StopConsumer()
--- static/js/commit_socket.js
+++ static/js/commit_socket.js
@@ -8,19 +8,37 @@ socket.onopen=()=>{
 }
 
 socket.onmessage=(e)=>{
-    console.log("Message received from the server")
+    console.log("Message received from the server", e)
+    data = JSON.parse(e.data)
+    console.log(data)
+    if (data.new_commit == "Unfollow"){
+        commitBtn.style.backgroundColor =  "var(--button-background)"
+        commitBtn.innerHTML = data.new_commit
+        commitBtn.value = `${data.new_commit} ${data.username}`
+    }
+    else if(data.new_commit == "Follow"){
+        commitBtn.style.backgroundColor = "var(--button-background)"
+        commitBtn.innerHTML = data.new_commit
+        commitBtn.value = `${data.new_commit} ${data.username}`
+    }
+    else{
+        alert("Sorry! Someting error happend!")
+    }
 }
 
 commitForm.addEventListener("submit", (e)=>{
+    commitBtn.style.backgroundColor = "grey"
     e.preventDefault()
     let commits = commitBtn.value
     commits = commits.split(" ")
+
     let commit = JSON.stringify(
         {
-            "username":commits[0],
-            "commit":commits[1]
+            "username":commits[1],
+            "commit":commits[0]
         })
     socket.send(commit)
+
 });
 
 
